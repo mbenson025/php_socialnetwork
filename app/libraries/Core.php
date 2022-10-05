71ca822f1b2f95dce087103e96ad9654bdacbd12
mbenson025 <mbenson025@gmail.com>
2022-10-05 18:10:13 -0500

    setup controller to take in value with correct syntax

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -11,7 +11,15 @@ class Core
 
   public function __construct()
   {
-    print_r($this->getUrl());
+    // print_r($this->getUrl());
+
+    $url = $this->getUrl();
+
+    //look in controllers for first value(index)
+    if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+      //if exists- set as controller
+      $this->currentController = ucwords($url[0]);
+    }
   }
 
   public function getURL()
